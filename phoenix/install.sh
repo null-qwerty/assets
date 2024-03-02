4b6cd310fbaba439ec34fffc7f02eaebd574a828
Haoyu Chen <null-qwerty@outlook.com>
2024-03-02 21:51:54 +0800

    update

---
+++ phoenix/install.sh
@@ -0,0 +1,8 @@
+sudo mkdir .phoenix_temp
+cd .phoenix_temp
+
+wget --no-check-certificate http://assets.null-qwerty.top/phoenix/RMCV2024-PHOENIX.zip
+unzip RMCV2024-PHOENIX.zip
+cd RMCV2024-PHOENIX
+
+bash scripts/install.sh
