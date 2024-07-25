ad03c5c094e01480087728c4d75e8e0ae0c302c3
Haoyu Chen <null-qwerty@outlook.com>
2024-07-26 02:31:25 +0800

    Update install.sh

--- phoenix/install.sh
+++ phoenix/install.sh
@@ -1,7 +1,7 @@
 sudo mkdir .phoenix_temp
 cd .phoenix_temp
 
-sudo wget --no-check-certificate http://assets.null-qwerty.top/phoenix/RMCV2024-PHOENIX.zip
+sudo wget --no-check-certificate https://github.com/HDU-PHOENIX/RMCV2024-PHOENIX/releases/download/v0.7.3/RMCV-PHOENIX-0.7.3-Linux.zip
 sudo unzip RMCV2024-PHOENIX.zip
 cd RMCV2024-PHOENIX
 
